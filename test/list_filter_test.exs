4462dec9695186e28182f108106d39e402cf03e0
alexandra <alexandrabsouz@gmail.com>
2022-01-07 19:14:07 -0300

    test: call/1 counts odd numbers

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -2,7 +2,15 @@ defmodule ListFilterTest do
   use ExUnit.Case
   doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "counts odd numbers" do
+      input = ["1", "3", "6", "43", "banana", "6", "abc"]
+
+      response = ListFilter.call(input)
+
+      expected_response = 3
+
+      assert response == expected_response
+    end
   end
 end
